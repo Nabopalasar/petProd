edbe77855b5ed8dfc96a59ec59f79b84b6546b51
Vyacheslav <vyacheslavburyy@gmail.com>
2023-07-01 14:59:35 +0300

    form path

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 // const dist = "./dist/";
-const dist = "Applications/MAMP/htdocs/project2";
+const dist = "/Applications/MAMP/htdocs/project2";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
